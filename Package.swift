53134a1b6605ca52554c08313e63641f33ce660a
seeaya <connorbarnesdenver@icloud.com>
2021-01-15 21:10:53 -0700

    Removed un-needed comments.

--- Package.swift
+++ Package.swift
@@ -1,23 +1,15 @@
 // swift-tools-version:5.2
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
 	name: "CoreSwiftVISA",
 	products: [
-		// Products define the executables and libraries produced by a package, and make them visible to other packages.
 		.library(
 			name: "CoreSwiftVISA",
 			targets: ["CoreSwiftVISA"]),
 	],
-	dependencies: [
-		// Dependencies declare other packages that this package depends on.
-		// .package(url: /* package url */, from: "1.0.0"),
-	],
 	targets: [
-		// Targets are the basic building blocks of a package. A target can define a module or a test suite.
-		// Targets can depend on other targets in this package, and on products in packages which this package depends on.
 		.target(
 			name: "CoreSwiftVISA",
 			dependencies: []),
@@ -25,5 +17,8 @@ let package = Package(
 			name: "CoreSwiftVISATests",
 			dependencies: ["CoreSwiftVISA"]),
 	],
+	// TODO: Can we target lower versions of Swift?
 	swiftLanguageVersions: [.v5]
 )
+
+
